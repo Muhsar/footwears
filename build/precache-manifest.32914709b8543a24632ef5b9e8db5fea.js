03f624a38fd9332f3e58c54c842cadf9b74513e5
Musa <jewbreel1@gmail.com>
2020-05-30 02:49:27 -0700

    edit client

---
+++ build/precache-manifest.32914709b8543a24632ef5b9e8db5fea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c54cb214a566503064ab0e695c888d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f84478de27aae4243bd9",
+    "url": "/static/css/2.76f4e2da.chunk.css"
+  },
+  {
+    "revision": "966fbc1860fe3534bf88",
+    "url": "/static/css/main.b5def7dc.chunk.css"
+  },
+  {
+    "revision": "f84478de27aae4243bd9",
+    "url": "/static/js/2.ddfe15fc.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.ddfe15fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966fbc1860fe3534bf88",
+    "url": "/static/js/main.4f10d5ca.chunk.js"
+  },
+  {
+    "revision": "af7b873c7a75ce3526f1",
+    "url": "/static/js/runtime-main.36e1cc88.js"
+  },
+  {
+    "revision": "213b94217a0bcd82da17b87984ea46db",
+    "url": "/static/media/img2.213b9421.jpg"
+  }
+]);
